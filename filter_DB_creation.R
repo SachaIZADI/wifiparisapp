f35ad4913f68e51cc1c33efda418d996057fb354
AMIR BENMAHJOUB <amirb@MacBook-Pro-de-AMIR.local>
2017-11-24 16:46:36 +0100

    adding a screenshot image

--- filter_DB_creation.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-library(wifiparis)
-filter_DB <- as.data.frame(Data_Viz_1) %>% select(- start_time) %>%  select(-duration) %>% unique()
-save(filter_DB, file = "filter_DB",compress = TRUE)
-
-load("filter_DB")
-filter_DB %>% View()
